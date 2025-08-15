74f0bbdcf663b48161db0151a87c6627c13ae959
Josh Hawkins <hawkins@users.noreply.github.com>
2025-08-15 12:31:56 -0400

    Add auto session to nvim

--- init.lua
+++ init.lua
@@ -994,7 +994,8 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  { import = 'custom.plugins' }, -- Where kickstart suggests installnig to
+  { import = 'plugins' }, -- Where Store installs to
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
