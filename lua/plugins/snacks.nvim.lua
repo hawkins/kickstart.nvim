62e32e9066b4f0cd211ef4fd30f8557dc0bfd4dc
Josh Hawkins <hawkins@users.noreply.github.com>
2025-08-27 18:06:25 -0400

    Add dashboard from snacks example

--- lua/plugins/snacks.nvim.lua
+++ lua/plugins/snacks.nvim.lua
@@ -12,6 +12,34 @@ return {
     },
     dashboard = {
       enabled = true,
+      sections = {
+        { section = 'header' },
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'echo $(pwd) , $(which python) | gum table --print --columns "Working Directory,Python Path" --border.foreground 0',
+          height = 5,
+          padding = 0,
+        },
+        { section = 'keys', gap = 1, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = 'git status --short --branch --renames',
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = 'startup' },
+      },
     },
     explorer = {
       enabled = true,
