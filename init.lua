f72e976e55e0042428c6715b0e20fba456cd512e
Josh Hawkins <hawkins@users.noreply.github.com>
2025-08-27 17:26:05 -0400

    Import individual mini components to avoid session

--- init.lua
+++ init.lua
@@ -945,8 +945,11 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+  -- Collection of various small independent plugins/modules
+  --  'echasnovski/mini.nvim',
+  -- Hawkins: Split to avoid mini.session
+  {
+    'echasnovski/mini.ai',
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -955,14 +958,22 @@ require('lazy').setup({
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-
+    end,
+  },
+  {
+    'echasnovski/mini.surround',
+    config = function()
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
-
+    end,
+  },
+  {
+    'echasnovski/mini.statusline',
+    config = function()
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
